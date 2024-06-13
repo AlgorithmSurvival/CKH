ee2bd59bd12dac9bd1e32f7ad194df17fabc262d
Ken <godsdy0410@naver.com>
2024-06-14 00:12:10 +0900

    11729_Solve

---
+++ 백준/Divide-and-conquer/11729_하노이 탑 이동 순서/11729_하노이 탑 이동 순서/11729_하노이 탑 이동 순서.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+using Process = vector<pair<int, int>>;
+
+void Hanoi(int N, int CurrentNum, int MiddleNum, int DestNum, int &Result, Process &Point)
+{
+    if (N != 1)
+    {
+        Hanoi(N - 1, CurrentNum, DestNum, MiddleNum, Result, Point);
+        Point.emplace_back(CurrentNum, DestNum);
+        Result++;
+        Hanoi(N - 1, MiddleNum, CurrentNum, DestNum, Result, Point);
+    }
+    else
+    {
+        Point.emplace_back(CurrentNum, DestNum);
+        Result++;
+    }
+
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+    int Result = 0;
+    Process Point;
+
+    Hanoi(N, 1, 2, 3, Result, Point);
+
+    cout << Result << '\n';
+    for (int i = 0; i < Point.size(); i++)
+    {
+        cout << Point[i].first << " " << Point[i].second << '\n';
+    }
+
+    return 0;
+}
