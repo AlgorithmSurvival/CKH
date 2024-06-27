a2ced65fcfd98773894ba3473421a21fabc9a347
Ken <godsdy0410@naver.com>
2024-06-27 21:22:00 +0900

    10610, 2875 Solve

---
+++ 백준/Greedy/10610_30/10610_30/10610_30.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    string N;
+    cin >> N;
+
+    vector<short> Numbers;
+    for (int i = 0; i < N.length(); i++)
+    {
+        Numbers.push_back(N[i] - 48);
+    }
+
+    sort(Numbers.begin(), Numbers.end());
+    if (Numbers[0] != 0)
+    {
+        cout << -1;
+        return 0;
+    }
+
+
+
+    int Remain = 0;
+    for (int i = 1; i < Numbers.size(); i++)
+    {
+        Remain = ((Remain + Numbers[i]) % 3);
+        Remain *= 10;
+    }
+    if (Remain != 0)
+    {
+        cout << -1;
+        return 0;
+    }
+
+
+
+    for (int i = Numbers.size() - 1; i >= 0; i--)
+    {
+        cout << Numbers[i];
+    }
+
+    return 0;
+}
---
+++ 백준/Greedy/2875_대회 or 인턴/2875_대회 or 인턴/2875_대회 or 인턴.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    float Woman = N;
+    int Man = M;
+    int KCount = K;
+
+    if (Woman / 2 <= 0 || Man <= 0)
+    {
+        cout << 0;
+        return 0;
+    }
+
+
+    while (KCount > 0)
+    {
+        if (Woman / 2 > Man)
+        {
+            Woman--;
+        }
+        else
+        {
+            Man--;
+        }
+        KCount--;
+    }
+
+    if (Woman / 2 > Man)
+    {
+        cout << Man;
+    }
+    else
+    {
+        cout << (int)(Woman / 2);
+    }
+    
+    return 0;
+}
+
