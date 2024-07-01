16dc569878c7a0984c35e15c74a0a4ba1abff475
Ken9903 <127284612+Ken9903@users.noreply.github.com>
2024-07-01 20:01:16 +0900

    1931_Solve

---
+++ 백준/Greedy/1931_회의실배정/1931_회의실배정/1931_회의실배정.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+using Data = vector<pair<int, int>>;
+
+bool Compare(pair<int, int> InFirst, pair<int, int> InSecond)
+{
+    if (InFirst.second == InSecond.second)
+    {
+        return InFirst.first < InSecond.first;
+    }
+    return InFirst.second < InSecond.second;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    Data Times;
+    int Result = 0;
+    int CurrentIndex = 0;
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        int Start, End;
+        cin >> Start >> End;
+        Times.emplace_back(Start, End);
+    }
+    sort(Times.begin(), Times.end(), Compare);
+
+    for (const auto Time : Times)
+    {
+        if (Time.first >= CurrentIndex)
+        {
+            Result++;
+            CurrentIndex = Time.second;
+        }
+    }
+
+    cout << Result;
+    return 0;
+}
+
