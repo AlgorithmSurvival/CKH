a66601f749aaa3c0c6e8c47dcd741dd999f94156
Ken9903 <127284612+Ken9903@users.noreply.github.com>
2024-07-02 21:32:17 +0900

    11399_Solve

---
+++ 백준/Greedy/11399_ATM/11399_ATM/11399_ATM.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+	int N;
+	cin >> N;
+
+	vector<short> Times;
+	for (int i = 0; i < N; i++)
+	{
+		short Input;
+		cin >> Input;
+		Times.emplace_back(Input);
+	}
+
+	sort(Times.begin(), Times.end());
+
+	int Sum = 0;
+	int Result = 0;
+	for (const auto Time : Times)
+	{
+		Sum += Time;
+		Result += Sum;
+	}
+
+	cout << Result;
+	return 0;
+}
