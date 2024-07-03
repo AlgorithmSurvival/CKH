7cd1bec17b62ffbe93b510b87f2a257803d3e0a1
Ken9903 <127284612+Ken9903@users.noreply.github.com>
2024-07-03 20:22:32 +0900

    1476_Solve

---
+++ 백준/Brute Force/1476_날짜 계산/1476_날짜 계산/1476_날짜 계산.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int E, S, M;
+    cin >> E >> S >> M;
+
+    int Result = 1;
+
+    while (!(E == 1 && S == 1 && M == 1))
+    {
+        E--;
+        S--;
+        M--;
+        if (E <= 0)
+        {
+            E = 15;
+        }
+        if (S <= 0)
+        {
+            S = 28;
+        }
+        if (M <= 0)
+        {
+            M = 19;
+        }
+        Result++;
+    }
+    
+    cout << Result;
+    return 0;
+}
