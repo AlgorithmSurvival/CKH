98e64cbd6fe3e0172d86942ca4b0262920847482
Ken9903 <127284612+Ken9903@users.noreply.github.com>
2024-07-15 23:37:09 +0900

    1697_Solve

---
+++ 백준/Brute Force/1697_숨바꼭질/1697_숨바꼭질/1697_숨바꼭질.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <math.h>
+using namespace std;
+constexpr int MAX_RANGE = 100001;
+
+int Bfs(vector<bool>& VisitVec, queue<int>& Queue, const int &K, const int &N)
+{
+    int Result = 0;
+    while (true)
+    {
+        queue<int> TempQueue;
+        while (!Queue.empty())
+        {
+            int CurrentPoint = Queue.front();
+            Queue.pop();
+
+            if (CurrentPoint == K)
+            {
+                return Result;
+            }
+
+
+            if (CurrentPoint == 0)
+            {
+                TempQueue.push(CurrentPoint + 1);
+                VisitVec[CurrentPoint + 1] = true;
+            }
+            else
+            {
+                if (CurrentPoint * 2 < MAX_RANGE && !VisitVec[CurrentPoint * 2])
+                {
+                    TempQueue.push(CurrentPoint * 2);
+                    VisitVec[CurrentPoint * 2] = true;
+                }
+                if (CurrentPoint + 1 < MAX_RANGE && !VisitVec[CurrentPoint + 1])
+                {
+                    TempQueue.push(CurrentPoint + 1);
+                    VisitVec[CurrentPoint + 1] = true;
+                }
+                if (CurrentPoint - 1 >= 0 && !VisitVec[CurrentPoint - 1])
+                {
+                    TempQueue.push(CurrentPoint - 1);
+                    VisitVec[CurrentPoint - 1] = true;
+                }
+            }
+        }
+        Queue = TempQueue;
+        Result++;
+    }
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<bool> VisitVec(MAX_RANGE, 0);
+    queue<int> Queue;
+    Queue.push(N);
+
+
+    cout << Bfs(VisitVec, Queue, K, N);
+    
+    return 0;
+}
